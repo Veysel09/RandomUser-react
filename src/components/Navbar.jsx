db6fee49eb1bbf737667bdd2970e4051a2e1b0f6
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-20 20:46:20 +0300

    Random

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="border border-dark-subtle mt-5  bg-dark">
+      <h1
+        className="text-danger text-center fs-1 fw-bold 
+      "
+      >
+        Random User
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar
+
+
+
+// import "bootstrap/dist/css/bootstrap.min.css"; index.js ye
