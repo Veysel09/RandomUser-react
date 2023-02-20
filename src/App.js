623bccd7b0203302509d83a5139e3f4b7b7424eb
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-20 21:00:37 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <div>
-APP
-  </div>
-  )
-  
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      <h1 className="text-danger shadow">Random User</h1>
+    </div>
+  );
+};
+
+export default Navbar;
